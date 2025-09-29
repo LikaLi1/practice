59129515c37ad91d30bbfa0bbe77082b6e18f816
Елизавета <167125218+LikaLi1@users.noreply.github.com>
2025-09-29 13:00:01 +0300

    pr9.cpp

---
+++ pr9.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+void task_1(){
+	std::cout << "Enter number: ";
+	int a;
+	std::cin >> a;
+	 if(a >= 0 && a % 10){
+		std::cout << "Your number is positive and double digit" << std::endl;
+	} else {
+		std::cout << "Your number is not positive" << std::endl;
+	}
+}
+
+void task_2(){
+  std::cout << "Enter number: ";
+	int a;
+	
+}
+
+void task_3(){
+	std::cout << "Enter password: ";
+	int pass;
+	std::cin >> pass;
+
+}
+
+void task_4(){
+	
+}
+
+void task_5(){
+	
+}
+
+void task_6(){
+	
+}
+
+void task_7(){
+	
+}
+
+void task_8(){
+	
+}
+
+void task_9(){
+	std::cout << "Enter month number: ";
+	int mon;
+	std::cin >> mon;
+  if(mon == 12){
+    std::cout << "Capricorn" << std::endl;
+  } else if(mon == 1){
+    std::cout << "Aquarius" << std::endl;
+  } else if(mon == 2){
+    std::cout << "Fish" << std::endl;
+  } else if(mon == 3){
+    std::cout << "Aries" << std::endl;
+  } else if (mon == 4){
+    std::cout << "Taurus" << std::endl;
+  } else if(mon == 5){
+    std::cout << "Twins" << std::endl;
+  } else if(mon == 6 ){
+    std::cout << "Twins" << std::endl;
+  } else if(mon == 7){
+    std::cout << "Lion" << std::endl;
+  } else if(mon == 8){
+    std::cout << "Virgo" << std::endl;
+  } else if(mon == 9){
+    std::cout << "Scales" << std::endl;
+  } else if(mon == 10){
+    std::cout << "Scales" << std::endl;
+  } else if(mon == 11){
+    std::cout << "Sagittarius" << std::endl;
+  }
+}
+
+void task_10(){
+	
+}
+
+int main(){
+	task_1();
+	task_2();
+	task_3();
+	task_4();
+	task_5();
+	task_6();
+	task_7();
+	task_8();
+	task_9();
+	task_10();
+
+	return 0;	
+}
