0246b32c15d6a6e0c580eba1df37502cf00b6509
Елизавета <167125218+LikaLi1@users.noreply.github.com>
2025-10-20 12:34:55 +0300

    pr18.cpp

--- pr18.cpp
+++ pr18.cpp
@@ -14,21 +14,31 @@ int main(){
     _setmode(_fileno(stderr), _O_U16TEXT);
     _setmode(_fileno(stdin), _O_U16TEXT);
 #else
-    std::ios_base::sync_with_stdio(false)
+    std::ios_base::sync_with_stdio(false);
         std::wcout.imbue(std::local("en_US.UTF-8"));
     std::wcin.imbue(std::local("en_US.UTF-8"));
 #endif
     std::wcout << L"Введите первое число: ";
     int a;
-    std::cin >> a;
+    std::wcin >> a;
 
     std::wcout << L"Введите второе число: ";
     int b;
-    std::cin >> b;
+    std::wcin >> b;
 
-    
+    int start = (a < b) ? a : b;
+    int end = (a > b) ? a : b;
+    int interval = start;
 
+    std::wcout << L"Интервал начинается с " << start << L" и до " << end << L": ";
+
+    while(interval <= end){
+        std::wcout << interval << L"; ";
+        ++interval;
     }
+
+    std::wcout << std::endl;
+
     return 0;
 }
 
