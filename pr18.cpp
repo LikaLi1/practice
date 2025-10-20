d845c5c90d7665c861e650a12208df59301f4ce4
Елизавета <167125218+LikaLi1@users.noreply.github.com>
2025-10-20 12:06:45 +0300

    pr18.cpp

---
+++ pr18.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <io.h>
+#include <fcntl.h>
+#endif
+int main(){
+#ifdef _WIN32
+    setlocale(LC_ALL, "");
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+#else
+    std::ios_base::sync_with_stdio(false)
+        std::wcout.imbue(std::local("en_US.UTF-8"));
+    std::wcin.imbue(std::local("en_US.UTF-8"));
+#endif
+    std::wcout << L"Введите первое число: ";
+    int a;
+    std::cin >> a;
+
+    std::wcout << L"Введите второе число: ";
+    int b;
+    std::cin >> b;
+
+    
+
+    }
+    return 0;
+}
+
