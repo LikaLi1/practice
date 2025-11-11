2864cbbaac3e7bc3b292a5a5070ebaa04696b5fb
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-11-11 12:14:12 +0300

    pr29.cpp

---
+++ pr29.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int distance_first_day = 15;
+    int distance = 0;
+
+    std::cout << "Number of days: ";
+    int d;
+    std::cin >> d;
+
+    for(int day = 1; day <= d; ++day){
+         distance =+ distance_first_day;
+         distance_first_day += 2;
+    }
+
+    std::cout << "The snail crawled: " << distance << std::endl;
+}
