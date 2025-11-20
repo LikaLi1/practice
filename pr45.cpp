9c51741ee6d28eb272e28909c4aa577f6dc8c2db
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-11-20 13:07:51 +0300

    pr45.cpp

---
+++ pr45.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[100];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int swapCount = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i -1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                swapCount++;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl; 
+    
+    cout << swapCount << endl;
+
+    return 0;
+}
